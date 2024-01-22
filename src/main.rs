13109efb31a60280338a5ecdbb8ea3b003f4049a
Chad Fernandez <developer@chadfernandez.me>
2024-01-22 14:41:05 +0800

    feat: add dynamic say input based on user input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use ferris_says::say; // from the previous step
+use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    let mut line = String::new();
+    println!("What will Ferris say? ");
+    std::io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line");
 
+    let stdout = stdout();
+    let width = line.chars().count();
     let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+
+    say(&line, width, &mut writer).unwrap();
 }
